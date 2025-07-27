e72150e5a3d6201b93323711ffb0a99fc6e5b68f
pillowbee <yan4ik345@yandex.by>
2025-07-27 13:15:53 +0300

    Deleted tanstack devtool

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,6 @@ import { queryClient } from "@/utils/queryClient"
 import CallsTable from "@/components/CallsTable/CallsTable.tsx"
 import styles from './App.module.css'
 
-window.__TANSTACK_QUERY_CLIENT__ = queryClient
-
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
